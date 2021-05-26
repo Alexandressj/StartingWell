6afae53cbba359600cdf3d1039d42985369946f9
Alexandre <pipoquinhas3s3@yahoo.com>
2021-05-26 02:22:12 -0300

    entao isso aqui é um arquivo mt random meu so de teste msm

---
+++ ambiente2.js
@@ -0,0 +1,14 @@
+/*var c = 10
+while (c <= 20) {
+    console.log(c)
+    c++
+}
+ */
+
+//-----------------------------------
+console.log('vai começar')
+for (var c = 10; c <= 20; c++) {
+    console.log(c)
+
+}
+console.log('FIM')
---
+++ ex015 copy/modelo.js
@@ -0,0 +1,21 @@
+function tabuada() {
+       let num = document.getElementById('txtn')
+       let tab = document.getElementById('seltab')
+       if (num.value.length == 0) {
+            alert(`Por favor,digite um numero`)
+        } else {
+            let n = Number(num.value)
+            let c = 1
+            tab.innerHTML = ''
+            while ( c <= 20 ) {
+                  let item = document.createElement('option')
+                  item.text = `${n} x ${c} = ${n*c}`
+                  item.value = `tab${c}` //importante pro PHP
+                  tab.appendChild(item)
+                  c++
+            }  
+    
+        }
+
+}
+
